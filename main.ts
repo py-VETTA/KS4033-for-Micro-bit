d56b3d2cb2c0aa01ca4092311412c6999be71091
py-VETTA <gcpivetta@gmail.com>
2026-08-28 16:21:59 -0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,18 @@
 namespace KS4033 {
 
-    //% block="teste KS4033"
-    export function teste(): void {
-        basic.showString("KS4033")
+    export enum Motor {
+        //% block="Motor Direito"
+        Direito,
+
+        //% block="Motor Esquerdo"
+        Esquerdo,
+
+        //% block="Todos"
+        Todos
     }
 
+    //% block="teste motor $motor"
+    export function testeMotor(motor: Motor): void {
+        basic.showNumber(motor)
+    }
 }
